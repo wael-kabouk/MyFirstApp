dddb2c7108aade96744e8f6bf9eac381be941d6f
Wael Kabouk <wael.kabouk2001@gmail.com>
2021-09-30 18:04:41 +0300

    MAD

---
+++ app/src/main/java/tr/edu/mu/ceng/wk/myfirstapp/MainActivity.java
@@ -0,0 +1,13 @@
+package tr.edu.mu.ceng.wk.myfirstapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
